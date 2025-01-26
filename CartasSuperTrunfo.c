635f4a9fe60cf4658fd3f185a173cd72250c0f3d
Alfabravonaval <alfabravonaval@gmail.com>
2025-01-26 21:31:42 +0000

    Desafio - Teste 1

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -18,5 +18,36 @@ int main() {
     // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
     // Exiba os valores inseridos para cada atributo da cidade, um por linha.
 
+    
+    int populacao, num_pontos_turisticos;
+    char cidade[20], estado[20], pais[20];
+    float area, pib;
+
+    printf("digite o nome do pais: \n");
+    scanf("%s", &pais);
+
+    printf("digite o nome do estado: \n");
+    scanf("%s", &estado);
+
+    printf("digite o nome da cidade: \n");
+    scanf("%d", &cidade);
+
+    printf("digite a populacao da cidade: \n");
+    scanf("%d", &populacao);
+
+    printf("digite o num_pontos_turisticos: \n");
+    scanf("%d", &num_pontos_turisticos);
+
+    printf("digite a area da cidade em km2: \n");
+    scanf("%f", &area); 
+
+    printf("digite o pib da cidade em bilhões): \n");
+    scanf("%f", &pib);
+
+    printf("nome do pais: %s - nome do estado: %s, pais, estado \n");
+    printf("populacao: %d - area: %f, populacao, area \n");
+    printf("num_pontos_turisticos: %d - pib: %f,num_pontos_turisticos,pib");
+    
+    
     return 0;
 }
